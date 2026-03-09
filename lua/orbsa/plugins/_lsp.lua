1f112376cc79ca39978b6b7fce0005dff4799a48
Eric Bell <eric.bell@wbdcontractor.com>
2026-03-09 13:13:29 -0600

    here we go

--- lua/orbsa/plugins/_lsp.lua
+++ lua/orbsa/plugins/_lsp.lua
@@ -1,5 +1,5 @@
--- TypeScript is handled by typescript-tools.nvim
--- No longer need lspconfig - using vim.lsp.config API instead
+-- Neovim 0.11+ built-in LSP configuration
+local capabilities = require('blink.cmp').get_lsp_capabilities()
 
 -- Rust 
 -- run Fly config on save
@@ -45,64 +45,55 @@ vim.g.rustaceanvim = {
     },
   },
 }
--- lspconfig.rust_analyzer.setup {
-  -- settings = {
-    -- ['rust-analyzer'] = {
-    --   cargo = {
-    --     loadOutDirsFromCheck = true,
-    --     features = "all"
-    --   },
-    --   files = {
-    --     excludeDirs = {
-    --       ".direnv",
-    --       ".git",
-    --       ".github",
-    --       ".gitlab",
-    --       "bin",
-    --       "web/node_modules",
-    --       "node_modules",
-    --       "target",
-    --       "venv",
-    --       ".venv",
-    --     },
-    --   }
-    -- },
-  -- },
--- }
-
--- Get LSP capabilities from blink.cmp
--- Using the native in-process language server (not nvim-cmp compat mode)
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('blink.cmp').get_lsp_capabilities(capabilities)
 vim.opt.completeopt = { "menu", "menuone", "noselect" }
 
--- HTML Language Server
-vim.lsp.config.html = {
-  cmd = { 'vscode-html-language-server', '--stdio' },
-  filetypes = { 'html' },
-  root_markers = { '.git' },
+-- HTML
+vim.lsp.config('html', {
   capabilities = capabilities,
-}
-vim.lsp.enable('html')
+})
 
--- CSS Language Server
-vim.lsp.config.cssls = {
-  cmd = { 'vscode-css-language-server', '--stdio' },
-  filetypes = { 'css', 'scss', 'less' },
-  root_markers = { '.git' },
+-- CSS
+vim.lsp.config('cssls', {
   capabilities = capabilities,
-}
-vim.lsp.enable('cssls')
+})
+
+-- Lua (for Neovim config)
+vim.lsp.config('lua_ls', {
+  capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file('', true),
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
+
+-- Nix
+vim.lsp.config('nil_ls', {
+  capabilities = capabilities,
+})
+
+vim.lsp.enable({ 'html', 'cssls', 'lua_ls', 'nil_ls' })
 
 -- RZLS
 require'rzls'.setup {
   capabilities = capabilities
 } 
 -- Roslyn
-vim.lsp.config.roslyn = {
+vim.lsp.config('roslyn', {
   cmd = {
-    -- "Microsoft.codeAnalysis.LanguageServer",
-    "roslyn-ls",
+    'roslyn-ls',
     '--logLevel=Information',
     '--extensionLogDirectory=' .. vim.fs.dirname(vim.lsp.get_log_path()),
     '--razorSourceGenerator=' .. vim.fs.joinpath(
@@ -124,30 +115,27 @@ vim.lsp.config.roslyn = {
     ),
     '--stdio',
   },
-  filetypes = { 'cs', 'razor' },
-  root_markers = { '*.sln', '*.csproj', '.git' },
   settings = {
     ["csharp|inlay_hints"] = {
-        csharp_enable_inlay_hints_for_implicit_object_creation = true,
-        csharp_enable_inlay_hints_for_implicit_variable_types = true,
-        csharp_enable_inlay_hints_for_lambda_parameter_types = true,
-        csharp_enable_inlay_hints_for_types = true,
-        dotnet_enable_inlay_hints_for_indexer_parameters = true,
-        dotnet_enable_inlay_hints_for_literal_parameters = true,
-        dotnet_enable_inlay_hints_for_object_creation_parameters = true,
-        dotnet_enable_inlay_hints_for_other_parameters = true,
-        dotnet_enable_inlay_hints_for_parameters = true,
-        dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
-        dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
-        dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
+      csharp_enable_inlay_hints_for_implicit_object_creation = true,
+      csharp_enable_inlay_hints_for_implicit_variable_types = true,
+      csharp_enable_inlay_hints_for_lambda_parameter_types = true,
+      csharp_enable_inlay_hints_for_types = true,
+      dotnet_enable_inlay_hints_for_indexer_parameters = true,
+      dotnet_enable_inlay_hints_for_literal_parameters = true,
+      dotnet_enable_inlay_hints_for_object_creation_parameters = true,
+      dotnet_enable_inlay_hints_for_other_parameters = true,
+      dotnet_enable_inlay_hints_for_parameters = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_differ_only_by_suffix = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_match_argument_name = true,
+      dotnet_suppress_inlay_hints_for_parameters_that_match_method_intent = true,
     },
     ["csharp|code_lens"] = {
-        dotnet_enable_references_code_lens = true,
+      dotnet_enable_references_code_lens = true,
     },
   },
-  on_attach = function(client)
-  end,
   capabilities = capabilities,
   handlers = require 'rzls.roslyn_handlers',
-}
+})
+
 vim.lsp.enable('roslyn')
